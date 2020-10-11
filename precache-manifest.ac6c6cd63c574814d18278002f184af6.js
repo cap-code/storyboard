070835c90536fe78964d437f1d1ff264a35ae2e1
cap-code <advaith007cube@gmail.com>
2020-10-11 16:11:10 +0530

    Updates

---
+++ precache-manifest.ac6c6cd63c574814d18278002f184af6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526fdaf83557f48719a94bd71abae3c2",
+    "url": "/github.io/storyboard/index.html"
+  },
+  {
+    "revision": "e13e5e56b612f11d6758",
+    "url": "/github.io/storyboard/static/css/main.71d2010f.chunk.css"
+  },
+  {
+    "revision": "08f94f63f400d797bc70",
+    "url": "/github.io/storyboard/static/js/2.85528d2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/storyboard/static/js/2.85528d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13e5e56b612f11d6758",
+    "url": "/github.io/storyboard/static/js/main.52bc685a.chunk.js"
+  },
+  {
+    "revision": "2c5c05a97e6c29429c5e",
+    "url": "/github.io/storyboard/static/js/runtime-main.e8d06efa.js"
+  },
+  {
+    "revision": "99898822ac7ab6b60d54f8305a4eab69",
+    "url": "/github.io/storyboard/static/media/logo1.99898822.png"
+  },
+  {
+    "revision": "ef2bdb0f66b898b97c56d2aa5f035c92",
+    "url": "/github.io/storyboard/static/media/moun.ef2bdb0f.jpg"
+  }
+]);
